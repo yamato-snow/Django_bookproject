08f79feaa9a3e61e7c1b92bbc5fa062e8b37cc73
yamato-snow <yamato2016baseball@icloud.com>
2024-05-05 08:50:16 +0900

    12時間目：本棚アプリケーション作成 応用編(4)

--- project/bookproject/book/models.py
+++ project/bookproject/book/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from .consts import MAX_RATE
+
+RATE_CHOICES = [(x, str(x)) for x in range(0, MAX_RATE + 1)]
 
 CATEGORY = (('business', 'ビジネス'), ('life', '生活'), ('other', 'その他'))
 
@@ -9,6 +12,16 @@ class Book(models.Model):
         max_length=100,
         choices = CATEGORY
     )
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    rate = models.IntegerField(choices=RATE_CHOICES)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- project/bookproject/book/urls.py
+++ project/bookproject/book/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('book/create/', views.CreateBookView.as_view(), name='create-book'),
     path('book/<int:pk>/delete/', views.DeleteBookView.as_view(), name='delete-book'),
     path('book/<int:pk>/update/', views.UpdateBookView.as_view(), name='update-book'),
+    path('book/<int:book_id>/review/', views.CreateReviewView.as_view(), name='review'),
 ]
