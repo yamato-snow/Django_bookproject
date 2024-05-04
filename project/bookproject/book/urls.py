2e1f5ee0d4ce4d49aea8fa83ca992efc4b41047a
yamato-snow <yamato2016baseball@icloud.com>
2024-05-05 08:23:15 +0900

    10時間目：本棚アプリケーション作成 応用編(2)

--- project/bookproject/book/urls.py
+++ project/bookproject/book/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('book/create/', views.CreateBookView.as_view(), name='create-book'),
     path('book/<int:pk>/delete/', views.DeleteBookView.as_view(), name='delete-book'),
     path('book/<int:pk>/update/', views.UpdateBookView.as_view(), name='update-book'),
+    path('logout/', views.logout_view, name='logout'),
 ]
