d2f9ec4307377e249f78dc4fd94e50fb8d3d5637
yamato-snow <yamato2016baseball@icloud.com>
2024-05-05 07:49:26 +0900

    4-11　UpdateViewを作成してブラウザ上でデータを編集できるようにしよう

--- project/bookproject/book/urls.py
+++ project/bookproject/book/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='detail-book'),
     path('book/create/', views.CreateBookView.as_view(), name='create-book'),
     path('book/<int:pk>/delete/', views.DeleteBookView.as_view(), name='delete-book'),
+    path('book/<int:pk>/update/', views.UpdateBookView.as_view(), name='update-book'),
 ]
--- project/bookproject/book/views.py
+++ project/bookproject/book/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+    )
 from .models import Book
 
 class ListBookView(ListView):
@@ -20,4 +26,10 @@ class CreateBookView(CreateView):
 class DeleteBookView(DeleteView):
     template_name = 'book/book_confirm_delete.html'
     model = Book
+    success_url = reverse_lazy('list-book')
+
+class UpdateBookView(UpdateView):
+    model = Book
+    fields = (['title', 'text', 'category'])
+    template_name = 'book/book_update.html'
     success_url = reverse_lazy('list-book')
