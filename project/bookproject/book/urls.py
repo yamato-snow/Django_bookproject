0d8c00780bb0ecc7ecfa99bdeac621dbfdfae208
yamato-snow <yamato2016baseball@icloud.com>
2024-05-05 07:27:20 +0900

    4-9　CreateViewでブラウザ上からデータを作成しよう

--- project/bookproject/book/urls.py
+++ project/bookproject/book/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/', views.ListBookView.as_view()),
-    path('book/<int:pk>/detail/', views.DetailBookView.as_view()),
+    path('book/', views.ListBookView.as_view(), name='list-book'),
+    path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='detail-book'),
+    path('book/create/', views.CreateBookView.as_view(), name='create-book'),
 ]
