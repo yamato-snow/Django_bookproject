f5f6b6ff9eb66911d972852ac0eec4e7ea7b86cc
yamato-snow <yamato2016baseball@icloud.com>
2024-05-05 10:21:48 +0900

    15時間目：本棚アプリケーション作成 応用編(7)

--- project/bookproject/book/views.py
+++ project/bookproject/book/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
+from django.db.models import Avg
 from django.views.generic import (
     ListView,
     DetailView,
@@ -10,10 +12,12 @@ from django.views.generic import (
     UpdateView,
     )
 from .models import Book, Review
+from .consts import ITEM_PER_PAGE
 
 class ListBookView(LoginRequiredMixin, ListView):
     template_name = '/book/book_list.html'
     model = Book
+    paginate_by = ITEM_PER_PAGE
 
 class DetailBookView(LoginRequiredMixin, DetailView):
     template_name = 'book/book_detail.html'
@@ -27,7 +31,7 @@ class CreateBookView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-
+        
         return super().form_valid(form)
 
 class DeleteBookView(LoginRequiredMixin, DeleteView):
@@ -40,7 +44,7 @@ class DeleteBookView(LoginRequiredMixin, DeleteView):
 
         if obj.user != self.request.user:
             raise PermissionDenied
-
+        
         return obj
 
 class UpdateBookView(LoginRequiredMixin, UpdateView):
@@ -53,15 +57,25 @@ class UpdateBookView(LoginRequiredMixin, UpdateView):
 
         if obj.user != self.request.user:
             raise PermissionDenied
-
+        
         return obj
-
+    
     def get_success_url(self):
         return reverse('detail-book', kwargs={'pk': self.object.id})
 
 def index_view(request):
-    object_list = Book.objects.order_by('category')
-    return render(request, 'book/index.html',{'object_list': object_list})
+    object_list = Book.objects.order_by('-id')
+    ranking_list = Book.objects.annotate(avg_rating=Avg('review__rate')).order_by('-avg_rating')
+    
+    paginator = Paginator(object_list, ITEM_PER_PAGE)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'book/index.html',
+        {'object_list': object_list, 'ranking_list': ranking_list, 'page_obj':page_obj },
+        )
 
 class CreateReviewView(LoginRequiredMixin, CreateView):
     model = Review
