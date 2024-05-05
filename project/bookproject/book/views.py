15102920b609f9f7bc895436c9f15b12d4fa9f34
yamato-snow <yamato2016baseball@icloud.com>
2024-05-05 09:18:53 +0900

    14時間目：本棚アプリケーション作成 応用編(6)

--- project/bookproject/book/views.py
+++ project/bookproject/book/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
@@ -9,36 +11,59 @@ from django.views.generic import (
     )
 from .models import Book, Review
 
-class ListBookView(ListView):
+class ListBookView(LoginRequiredMixin, ListView):
     template_name = '/book/book_list.html'
     model = Book
 
-class DetailBookView(DetailView):
+class DetailBookView(LoginRequiredMixin, DetailView):
     template_name = 'book/book_detail.html'
     model = Book
 
-class CreateBookView(CreateView):
+class CreateBookView(LoginRequiredMixin, CreateView):
     template_name = 'book/book_create.html'
     model = Book
     fields = ('title', 'text', 'category', 'thumbnail')
     success_url = reverse_lazy('list-book')
 
-class DeleteBookView(DeleteView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return super().form_valid(form)
+
+class DeleteBookView(LoginRequiredMixin, DeleteView):
     template_name = 'book/book_confirm_delete.html'
     model = Book
     success_url = reverse_lazy('list-book')
 
-class UpdateBookView(UpdateView):
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+
+        if obj.user != self.request.user:
+            raise PermissionDenied
+
+        return obj
+
+class UpdateBookView(LoginRequiredMixin, UpdateView):
     model = Book
     fields = ('title', 'text', 'category', 'thumbnail')
     template_name = 'book/book_update.html'
-    success_url = reverse_lazy('list-book')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+
+        if obj.user != self.request.user:
+            raise PermissionDenied
+
+        return obj
+
+    def get_success_url(self):
+        return reverse('detail-book', kwargs={'pk': self.object.id})
 
 def index_view(request):
     object_list = Book.objects.order_by('category')
     return render(request, 'book/index.html',{'object_list': object_list})
 
-class CreateReviewView(CreateView):
+class CreateReviewView(LoginRequiredMixin, CreateView):
     model = Review
     fields = ('book', 'title', 'text', 'rate')
     template_name = 'book/review_form.html'
@@ -47,10 +72,10 @@ class CreateReviewView(CreateView):
         context = super().get_context_data(**kwargs)
         context['book'] = Book.objects.get(pk=self.kwargs['book_id'])
         return context
-
+    
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-
+    
     def get_success_url(self):
         return reverse('detail-book', kwargs={'pk': self.object.book.id})
